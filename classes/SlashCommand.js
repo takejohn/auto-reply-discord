b03b3fcd2966988e50d19232a6e0870b13cb0e89
takejohn <105504345+takejohn@users.noreply.github.com>
2023-12-10 01:55:41 +0900

    自動リプライシステムの追加

---
+++ classes/SlashCommand.js
@@ -0,0 +1,67 @@
+const { SlashCommandBuilder, RESTPostAPIChatInputApplicationCommandsJSONBody, REST, Routes, Client, ChatInputCommandInteraction } = require("discord.js");
+
+/**
+ * スラッシュコマンドを表すオブジェクト
+ */
+class SlashCommand {
+
+    /**
+     * @type {RESTPostAPIChatInputApplicationCommandsJSONBody}
+     */
+    #jsonBody;
+
+    /**
+     * @type {((interaction: ChatInputCommandInteraction<CacheType>) => void) | undefined}
+     */
+    #executeFunc;
+
+    /**
+     * スラッシュコマンドを表すオブジェクトを作成する。
+     * @param {SlashCommandBuilder} builder スラッシュコマンドの元となるビルダーオブジェクト
+     * @param {((interaction: ChatInputCommandInteraction<CacheType>) => void) | undefined} executeFunc コマンドの実行時に行う処理
+     */
+    constructor(builder, executeFunc) {
+        this.#jsonBody = builder.toJSON();
+        this.#executeFunc = executeFunc;
+    }
+
+    /**
+     * コマンドを実行する。
+     * @param {ChatInputCommandInteraction<CacheType>} interaction interactionCreate イベントの内容
+     */
+    async execute(interaction) {
+        this.#executeFunc(interaction);
+    }
+
+    /**
+     * ギルドにコマンドを追加する。
+     * @param {REST} rest エンドポイントのハンドラを管理する REST オブジェクト
+     * @param {Client} client bot のクライアント
+     * @param {string} guildId コマンドを追加するギルド ID
+     * @param {...SlashCommand} commands 追加するコマンド
+     */
+    static async putGuildCommands(rest, client, guildId, ...commands) {
+        await rest.put(Routes.applicationGuildCommands(client.application.id, guildId), {
+            body: Array.from(commands).map(command => command.#jsonBody)
+        });
+
+        /** @type {Map<string, SlashCommand>} */
+        const nameCommandMap = new Map();
+        for (const command of commands) {
+            nameCommandMap.set(command.#jsonBody.name, command);
+        }
+        client.on("interactionCreate", async function(interaction) {
+            if (!interaction.isChatInputCommand()) {
+                return;
+            }
+            const command = nameCommandMap.get(interaction.commandName);
+            if (command === void 0) {
+                return;
+            }
+            await command.execute(interaction);
+        });
+    }
+
+}
+
+module.exports = SlashCommand;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes } = require("discord.js");
 const fs = require("fs");
 const sqlite3 = require("sqlite3");
+const SlashCommand = require("./classes/SlashCommand");
+const crypto = require("crypto");
 
 /**
  * sqlite3 データベース上で SQL クエリを実行する。
@@ -30,24 +32,79 @@ const database = new sqlite3.Database("./data/auto-reply.db");
 
 (async function() {
     await runSqlAsync(database, `PRAGMA foreign_keys = true;`);
-    await runSqlAsync(database, `CREATE TABLE IF NOT EXISTS reply_messages(
-        id integer primary key autoincrement,
-        message text
+    await runSqlAsync(database, `CREATE TABLE IF NOT EXISTS reply_contents(
+        reply_id char(36),
+        guild_id char(20),
+        content text
     );`);
     await runSqlAsync(database, `CREATE TABLE IF NOT EXISTS reply_keywords(
-        keyword text primary key,
-        message_id integer,
-        foreign key (message_id) references reply_messages(id)
+        keyword text,
+        reply_id char(36),
+        primary key (keyword, reply_id)
     );`);
-    return database;
 })();
 
 const client = new Client({
     intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b)
 });
 
+/**
+ * @type {{token: string, guild_ids: string[]}}
+ */
 const config = JSON.parse(fs.readFileSync("./data/config.json", "utf-8"));
 
-client.on("ready", () => console.log(`Logged in as ${client.user.tag}`));
+const rest = new REST({version: 10}).setToken(config.token);
+
+const commandAutoReply = new SlashCommand(new SlashCommandBuilder()
+    .setName("add-reply")
+    .setDescription("自動リプライのパターンを追加します。")
+    .addStringOption(option => option
+        .setName("keyword")
+        .setDescription("リプライ対象のメッセージ")
+        .setRequired(true))
+    .addStringOption(option => option
+        .setName("content")
+        .setDescription("リプライの内容")
+        .setRequired(true)),
+    async function(interaction) {
+        const guildId = interaction.guildId;
+        const keyword = interaction.options.getString("keyword");
+        const content = interaction.options.getString("content");
+        if (content.length == 0) {
+            interaction.reply("空の内容を設定することはできません。");
+            return;
+        }
+        if ((await runSqlAsync(database, `SELECT keyword FROM reply_keywords WHERE reply_id IN (
+            SELECT reply_id FROM reply_contents WHERE guild_id = ?
+        ) AND keyword = ?;`, guildId, keyword)).length != 0) {
+            interaction.reply("そのキーワードはすでに設定されています。");
+            return;
+        }
+        const replyId = crypto.randomUUID();
+        await runSqlAsync(database, `INSERT INTO reply_contents(reply_id, guild_id, content) VALUES (?, ?, ?)`, replyId, guildId, content);
+        await runSqlAsync(database, `INSERT INTO reply_keywords(keyword, reply_id) VALUES (?, ?)`, keyword, replyId);
+        interaction.reply(`自動リプライのパターンを追加しました。\n\`${keyword}\` → \`${content}\``);
+    });
+
+client.on("ready", function() {
+    console.log(`Logged in as ${client.user.tag}`);
+    for (const guildId of config.guild_ids) {
+        SlashCommand.putGuildCommands(rest, client, guildId, commandAutoReply);
+    }
+});
+
+client.on("messageCreate", async function(message) {
+    if (message.author.bot) {
+        return;
+    }
+    const guildId = message.guildId;
+    const searchResult = await runSqlAsync(database, `
+        SELECT content FROM reply_contents WHERE guild_id = ? AND reply_id IN (
+            SELECT reply_id FROM reply_keywords WHERE keyword = ?
+        );`, guildId, message.content);
+    if (searchResult.length >= 1) {
+        message.reply(searchResult[0].content);
+    }
+});
 
 client.login(config.token);
